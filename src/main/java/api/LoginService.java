3e2e2ee6fbed1ec43a695a54aa03b52aa85342e4
Monroena <Magnusneedham@gmail.com>
2022-01-10 23:00:40 +0100

    EKG data til java

--- src/main/java/api/LoginService.java
+++ src/main/java/api/LoginService.java
@@ -6,6 +6,10 @@ import model.LoginData;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+
+
+
+
 @Path("login")
 @Produces({MediaType.TEXT_PLAIN})
 public class LoginService {
@@ -16,4 +20,23 @@ public class LoginService {
         return LoginController.getLoginControllerOBJ().doLogin(loginData);
     }
 
-}
+    @POST
+    public String postEKGData(String EKGdata) {
+
+        //System.out.println(EKGdata);
+
+
+        String[] split = EKGdata.split(",");
+
+        for (int i = 0; i < split.length; i++) {
+
+
+
+            System.out.println(split[i]);
+            // System.out.println("Done");
+        }
+            return EKGdata;
+
+    }
+    }
+
