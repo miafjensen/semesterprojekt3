69b233c8fd069a1f5c5fb7fa0eba3f229982248c
miafjensen <78786706+miafjensen@users.noreply.github.com>
2022-01-15 12:10:24 +0100

    med rangeslider funktion på EKG grafen igen!

--- src/main/webapp/Resultatside.js
+++ src/main/webapp/Resultatside.js
@@ -1,4 +1,6 @@
 var chart = ""
+let rangevalues = []
+let labels = []
 
 async function HentEkgData() {
     // Serialiser formen til js-objekt
@@ -16,9 +18,10 @@ async function HentEkgData() {
 
     let values = await res.json()
     //console.log(json)
-    let labels = []
+    rangevalues = values
     for (var i = 0; i < values.length; i++)
         labels.push("" + i)
+    //rangevalues.push(values)
     //Array(json.length).fill("")
 
     if (chart?.destroy) {
@@ -47,58 +50,59 @@ async function HentEkgData() {
         },
     });
 
-}
+    function getVals() {
+        // Get slider values
+        var parent = this.parentNode;
+        var slides = parent.getElementsByTagName("input");
+        var min = parseFloat(slides[0].value);
+        var max = parseFloat(slides[1].value);
+        // Neither slider will clip the other, so make sure we determine which is larger
+        if (min > max) {
+            var tmp = max;
+            max = min;
+            min = tmp;
+        }
 
-/* nedenstående er baseret på eksempel fra https://jsfiddle.net/gh7qb4ud/1/ */
-function getVals() {
-    // Get slider values
-    var parent = this.parentNode;
-    var slides = parent.getElementsByTagName("input");
-    var min = parseFloat(slides[0].value);
-    var max = parseFloat(slides[1].value);
-    // Neither slider will clip the other, so make sure we determine which is larger
-    if (min > max) {
-        var tmp = max;
-        max = min;
-        min = tmp;
+        var label = [];
+        var value = [];
+
+        label = JSON.parse(JSON.stringify(labels)).slice(min, max);
+
+        //var datasets = Data.datasets;
+        // IF YOU HAVE MULTIPLE SERIESES
+
+        // ChartObj.data.labels = label;
+        // for (var i = 0; i < datasets.length; i++) {
+        //     values = datasets[i].data.slice(min, max);
+        //     ChartObj.data.datasets[i].data = values;
+        // }
+        // ChartObj.update();
+        value = JSON.parse(JSON.stringify(rangevalues)).slice(min, max);
+        chart.data.labels = label;
+        chart.data.datasets[0].data = value;
+        chart.update();
+
+        var displayElement = parent.getElementsByClassName("rangeValues")[0];
+        displayElement.innerHTML = "Min : " + min + " Max : " + max;
     }
 
-    var label = [];
-    var value = [];
-
-    label = JSON.parse(JSON.stringify(labels)).slice(min, max);
-
-    //var datasets = Data.datasets;
-    // IF YOU HAVE MULTIPLE SERIESES
-
-    // ChartObj.data.labels = label;
-    // for (var i = 0; i < datasets.length; i++) {
-    //     values = datasets[i].data.slice(min, max);
-    //     ChartObj.data.datasets[i].data = values;
-    // }
-    // ChartObj.update();
-    value = JSON.parse(JSON.stringify(values)).slice(min, max);
-    chart.data.labels = label;
-    chart.data.datasets[0].data = value;
-    chart.update();
-
-    var displayElement = parent.getElementsByClassName("rangeValues")[0];
-    displayElement.innerHTML = "Min : " + min + " Max : " + max;
-}
-
 // Initialize Sliders
-var sliderSections = document.getElementsByClassName("range-slider");
-for (var x = 0; x < sliderSections.length; x++) {
-    var sliders = sliderSections[x].getElementsByTagName("input");
-    for (var y = 0; y < sliders.length; y++) {
-        if (sliders[y].type === "range") {
-            sliders[y].oninput = getVals;
-            sliders[y].max = JSON.parse(JSON.stringify(labels)).length;
-            // Manually trigger event first time to display values
-            sliders[y].oninput();
+    var sliderSections = document.getElementsByClassName("range-slider");
+    for (var x = 0; x < sliderSections.length; x++) {
+        var sliders = sliderSections[x].getElementsByTagName("input");
+        for (var y = 0; y < sliders.length; y++) {
+            if (sliders[y].type === "range") {
+                sliders[y].oninput = getVals;
+                sliders[y].max = JSON.parse(JSON.stringify(labels)).length;
+                // Manually trigger event first time to display values
+                sliders[y].oninput();
+            }
         }
     }
 }
+
+/* nedenstående er baseret på eksempel fra https://jsfiddle.net/gh7qb4ud/1/  til range sliders*/
+
 
 function findEKG() {
 
