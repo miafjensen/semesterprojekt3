0c17c8d9a36c79b4963b2b5d4e2fab3c0ccc5542
miafjensen <78786706+miafjensen@users.noreply.github.com>
2022-01-17 14:46:46 +0100

    bøvl med xml

--- src/main/java/model/AftaleListe.java
+++ src/main/java/model/AftaleListe.java
@@ -7,13 +7,15 @@ import java.util.List;
 @XmlRootElement(name = "aftaleListe")
 @XmlSeeAlso(Aftale.class)
 @XmlAccessorType(XmlAccessType.FIELD)
-//@jakarta.xml.bind.annotation.XmlRootElement(name="aftaleListe")
-//@jakarta.xml.bind.annotation.XmlSeeAlso(Aftale.class)
+
 public class AftaleListe {
 
     @XmlElement(name = "aftale")
-    //@jakarta.xml.bind.annotation.XmlElement(name="aftale")
     List<Aftale> aftaleListe = new ArrayList<>();
+
+    public void setAftaleListe(List<Aftale> aftaleListe) {
+        this.aftaleListe = aftaleListe;
+    }
 
     public List<Aftale> getAftaler() {
         return aftaleListe;
